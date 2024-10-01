a16c4c6eed2c20bd3683c70d29710900a1819020
unknown <19521407@gm.uit.edu.vn>
2024-10-01 08:51:39 +0700

    update code

--- src/app/(public)/(auth)/login/login-form.tsx
+++ src/app/(public)/(auth)/login/login-form.tsx
@@ -10,11 +10,15 @@ import {
 import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { toast } from "@/hooks/use-toast";
+import { handleErrorApi } from "@/lib/utils";
+import { useLoginMutation } from "@/queries/useAuth";
 import { LoginBody, LoginBodyType } from "@/schemaValidations/auth.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
 export default function LoginForm() {
+  const loginMutation = useLoginMutation();
   const form = useForm<LoginBodyType>({
     resolver: zodResolver(LoginBody),
     defaultValues: {
@@ -22,6 +26,20 @@ export default function LoginForm() {
       password: "",
     },
   });
+
+  const onSubmit = async (data: LoginBodyType) => {
+    if (loginMutation.isPending) return;
+    try {
+      const result = await loginMutation.mutateAsync(data);
+      console.log("result", result);
+      toast({ description: result.payload.message });
+    } catch (error) {
+      handleErrorApi({
+        error,
+        setError: form.setError,
+      });
+    }
+  };
 
   return (
     <Card className="mx-auto max-w-sm">
@@ -36,6 +54,9 @@ export default function LoginForm() {
           <form
             className="space-y-2 max-w-[600px] flex-shrink-0 w-full"
             noValidate
+            onSubmit={form.handleSubmit(onSubmit, (err) => {
+              console.log(err);
+            })}
           >
             <div className="grid gap-4">
               <FormField
