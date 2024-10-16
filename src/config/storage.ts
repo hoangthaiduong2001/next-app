11e828b01ff2adeb56758f00a9ac42419e6f2e2e
unknown <19521407@gm.uit.edu.vn>
2024-10-16 21:26:14 +0700

    remove pakage

--- src/config/storage.ts
+++ src/config/storage.ts
@@ -2,12 +2,10 @@ import { ACCESS_TOKEN, REFRESH_TOKEN, USER_INFO } from "@/constants/auth";
 import { toast } from "@/hooks/use-toast";
 import {
   LoginResType,
-  RefreshTokenBodyType,
   RefreshTokenResType,
 } from "@/schemaValidations/auth.schema";
 import { IPlainObject } from "@/types/common";
-import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
-import Cookies from "js-cookie";
+import axios, { AxiosError, AxiosInstance } from "axios";
 export const persistTokenAndAccount = (data: LoginResType) => {
   const {
     data: { accessToken, refreshToken, account },
@@ -34,8 +32,6 @@ export const getRefreshToken = () => {
   let token = "";
   if (typeof window !== "undefined") {
     token = localStorage.getItem(REFRESH_TOKEN) as string;
-  } else {
-    token = Cookies.get(REFRESH_TOKEN) || "";
   }
   return token;
 };
@@ -66,38 +62,6 @@ export const axiosHttp = (
     headers,
   });
   return axiosInstance;
-};
-
-export const refreshTokenFn = async (): Promise<
-  RefreshTokenResType | undefined
-> => {
-  const refreshToken = getRefreshToken();
-  console.log("refreshToken", refreshToken);
-  try {
-    console.log("456");
-    await axios
-      .post<
-        RefreshTokenResType,
-        AxiosResponse<RefreshTokenResType, RefreshTokenBodyType>,
-        RefreshTokenBodyType
-      >("/auth/refresh-token", {
-        refreshToken,
-      })
-      .catch((err) => console.log("loi o day", err));
-    const res = await axios.post<
-      RefreshTokenResType,
-      AxiosResponse<RefreshTokenResType, RefreshTokenBodyType>,
-      RefreshTokenBodyType
-    >("/auth/refresh-token", {
-      refreshToken,
-    });
-    const { data } = res;
-    console.log("refretokenres", res);
-    persistToken(data);
-    return data;
-  } catch (error) {
-    console.log("error in refreserToken", error);
-  }
 };
 
 export const clearCacheAndNavigateToLoginPage = (
