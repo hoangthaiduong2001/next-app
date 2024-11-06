a12026676479d6d16b3ed60701cf7634df99e03c
hduytins <duytin.huynh@btaskee.com>
2024-11-06 16:44:03 +0700

    update code

--- src/apiRequest/media.ts
+++ src/apiRequest/media.ts
@@ -2,16 +2,7 @@ import http from "@/config/http";
 import { UploadImageResType } from "@/schemaValidations/media.schema";
 
 const mediaApiRequest = {
-  serverUploadImage: (accessToken: string, formData: FormData) =>
-    http.post<UploadImageResType>("/media/upload", formData, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    }),
-  clientUploadImage: (formData: FormData) =>
-    http.post<UploadImageResType>("/api/media/upload", formData, {
-      baseUrl: "",
-    }),
+  uploadImage: (formData: FormData) => http.post<UploadImageResType>("/media/upload", formData),
 };
 
 export default mediaApiRequest;
