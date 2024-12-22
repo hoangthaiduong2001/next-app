c80c9b389dcceed2141a3f1e6e94a87f6a01dea4
hduytins <duytin.huynh@btaskee.com>
2024-12-22 12:32:06 +0700

    update code

---
+++ src/apiRequest/guest.ts
@@ -0,0 +1,60 @@
+import http from "@/config/http";
+import {
+  LoginResType,
+  LogoutBodyType,
+  RefreshTokenBodyType,
+  RefreshTokenResType,
+} from "@/schemaValidations/auth.schema";
+import { MessageResType } from "@/schemaValidations/common.schema";
+import {
+  GuestLoginBodyType,
+  GuestLoginResType,
+} from "@/schemaValidations/guest.schema";
+
+type TRefreshTokenKey = {
+  status: number;
+  response: RefreshTokenResType;
+};
+
+export const guestApiRequest = {
+  refreshTokenRequest: null as Promise<TRefreshTokenKey> | null,
+  serverLogin: (body: GuestLoginBodyType) =>
+    http.post<GuestLoginResType>("/guest/auth/login", body),
+  serverLogout: (body: LogoutBodyType & { accessToken: string }) =>
+    http.post<LoginResType>(
+      "/guest/auth/logout",
+      {
+        refreshToken: body.refreshToken,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${body.accessToken}`,
+        },
+      }
+    ),
+  serverRefreshToken: (body: RefreshTokenBodyType) =>
+    http.post<RefreshTokenResType>("/guest/auth/refresh-token", body),
+  clientLogin: (body: GuestLoginBodyType) =>
+    http.post<GuestLoginResType>("/api/guest/auth/login", body, {
+      baseUrl: "",
+    }),
+  clientLogout: () =>
+    http.post<MessageResType>("/api/guest/auth/logout", null, {
+      baseUrl: "",
+    }),
+  async clientRefreshToken() {
+    if (this.refreshTokenRequest) {
+      return this.refreshTokenRequest;
+    }
+    this.refreshTokenRequest = http.post<RefreshTokenResType>(
+      "api/guest/auth/refresh-token",
+      null,
+      {
+        baseUrl: "",
+      }
+    );
+    const result = await this.refreshTokenRequest;
+    this.refreshTokenRequest = null;
+    return result;
+  },
+};
---
+++ src/app/(public)/tables/[number]/page.tsx
@@ -0,0 +1,5 @@
+import GuestLoginForm from "@/app/(public)/tables/[number]/guestLoginForm";
+
+export default function TableNumberPage() {
+  return <GuestLoginForm />;
+}
