9cb49fb5deee6b268a00df27eec4962d4c83df47
hduytins <duytin.huynh@btaskee.com>
2024-12-24 13:42:04 +0700

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
+import { decodeToken } from "./config/utils";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants/auth";
+import { Role } from "./constants/type";
 
-const privatePaths = ["/manage"];
+const managePaths = ["/manage"];
+const guestPaths = ["/guest"];
 const publicPaths = ["/login"];
+const privatePaths = [...managePaths, ...guestPaths];
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
@@ -13,22 +17,33 @@ export function middleware(request: NextRequest) {
     url.searchParams.set("clearTokens", "true");
     return NextResponse.redirect(url);
   }
-  if (publicPaths.some((path) => pathname.startsWith(path)) && refreshToken) {
-    return NextResponse.redirect(new URL("/", request.url));
+  if (refreshToken) {
+    if (publicPaths.some((path) => pathname.startsWith(path))) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+    if (
+      privatePaths.some((path) => pathname.startsWith(path)) &&
+      !accessToken
+    ) {
+      const url = new URL("/refresh-token", request.url);
+      url.searchParams.set(REFRESH_TOKEN, refreshToken);
+      url.searchParams.set("redirect", pathname);
+      return NextResponse.redirect(url);
+    }
+    const role = decodeToken(refreshToken).role;
+    if (
+      (role === Role.Guest &&
+        managePaths.some((path) => pathname.startsWith(path))) ||
+      (role !== Role.Guest &&
+        guestPaths.some((path) => pathname.startsWith(path)))
+    ) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
   }
-  if (
-    privatePaths.some((path) => pathname.startsWith(path)) &&
-    !accessToken &&
-    refreshToken
-  ) {
-    const url = new URL("/refresh-token", request.url);
-    url.searchParams.set(REFRESH_TOKEN, refreshToken);
-    url.searchParams.set("redirect", pathname);
-    return NextResponse.redirect(url);
-  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/manage/:path*", "/login"],
+  matcher: ["/manage/:path*", "/guest/:paths*", "/login"],
 };
